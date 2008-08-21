e1faaec16bc33a1ddce3b4f5d22edff559afa453
mmwise <mmwise@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2008-08-21 00:16:44 +0000

    adding up to date xml and code

--- motor_qualification/src/gui/gui.py
+++ motor_qualification/src/gui/gui.py
@@ -4,9 +4,12 @@
 import rostools
 rostools.update_path('mechanism_control')
 rostools.update_path('generic_controllers')
+rostools.update_path('std_srvs')
 
 from mechanism_control.srv import *
 from generic_controllers.srv import *
+from mechanism_control.msg import *
+from std_srvs.srv import Empty, EmptyRequest
 import rostools.packspec as packspec
 import rospy, sys
 import os, signal, popen2
@@ -21,19 +24,18 @@ class RosFrame(wx.Frame):
    
     panel = wx.Panel(self, -1)
     sizer = wx.GridBagSizer(4, 4)
-
+    self.pid=None
     self.testing = 0 
-    self.data = [(1,2), (2,3), (3,5), (4,6), (5,8), (6,8), (10,10)]
-
+    #self.data = [(1,2), (2,3), (3,5), (4,6), (5,8), (6,8), (10,10)]
+    self.data =[(0,0)]
+    self.data2 =[(0,0)]
     text1 = wx.StaticText(panel, -1, 'Motor Response')
     sizer.Add(text1, (0, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
 
 
-    client = plot.PlotCanvas(panel,-1)
-    markers = plot.PolyMarker(self.data, legend='', colour='blue', marker='circle', size=1)
-    gc = plot.PlotGraphics([markers], 'Motor Data', 'Time', 'Velocity')
-    client.Draw(gc, xAxis=(0,15), yAxis=(0,15))
-    sizer.Add(client, (1, 0), (5, 3), wx.EXPAND | wx.LEFT | wx.RIGHT, 5)                
+    self.client = plot.PlotCanvas(panel,-1)
+    self.drawPlot()
+    sizer.Add(self.client, (1, 0), (5, 3), wx.EXPAND | wx.LEFT | wx.RIGHT, 5)                
 
     text2 = wx.StaticText(panel, -1, 'Status')
     sizer.Add(text2, (6, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
@@ -55,8 +57,26 @@ class RosFrame(wx.Frame):
 
     self.Bind(wx.EVT_BUTTON, self.start, id=106)
     self.Bind(wx.EVT_BUTTON, self.stop, id=107)
+    self.Bind(wx.EVT_IDLE, self.onIdle)
     self.Centre()
+  
+  def drawPlot(self):
+    #markers = plot.PolyMarker(self.data, legend='', colour='blue', marker='circle', size=1)
+    markers = plot.PolyLine(self.data)
+    markers2 = plot.PolyLine(self.data2, colour='blue')
+    gc = plot.PlotGraphics([markers,markers2], 'Motor Data', 'Time', 'Velocity')
+    self.client.Draw(gc)
      
+  def callback(self, mechanism_state):
+    self.data.append((mechanism_state.time, mechanism_state.joint_states[1].velocity))
+    self.data2.append((mechanism_state.time, mechanism_state.joint_states[0].velocity))
+    self.data = self.data[-100:]
+    self.data2 = self.data2[-100:]
+
+  def onIdle(self, event):
+    event.RequestMore(True)
+    self.drawPlot()
+    
   def start(self, event):
     if self.testing == 0:
       dlg = wx.TextEntryDialog(self, 'Scan the motor','Motor Model')
@@ -74,10 +94,15 @@ class RosFrame(wx.Frame):
     dlg.Destroy()
 
   def stop(self, event):
-    end_test(self.pid)
-    dlg = wx.MessageDialog(self, 'Ending Test','User Hault', wx.OK|wx.ICON_HAND)
-    self.testing=0
-    dlg.ShowModal()
+    if(self.testing):
+      end_test()
+      dlg = wx.MessageDialog(self, 'Ending Test','User Hault', wx.OK|wx.ICON_HAND)
+      self.testing=0
+      dlg.ShowModal()
+    else:
+      self.error('NO_TEST')
+    return 
+   
     
   def check_motor(self, motor):
     if motor[2:7] == '03009':
@@ -101,8 +126,11 @@ class RosFrame(wx.Frame):
     self.text.SetDefaultStyle(wx.TextAttr(wx.RED))
     if error_code == 'NOT_MOTOR':
       self.text.AppendText("ERROR: The object you scanned is not a motor or the motor can not be evaluated by this test.\n")
+    elif error_code == 'NO_TEST':
+      self.text.AppendText("ERROR: Cannot Stop. No test in progress.\n")
     else:
       self.text.AppendText("ERROR: Unknown Error\n")
+    return
         
 class RosApp(wx.App):
   def OnInit(self):
@@ -129,11 +157,11 @@ def kill_controller(name):
 
 def start_test(motor,frame):
   xmlFile ='WG_'+motor[2:7]+'.xml'
-  sub = popen2.Popen3('botherder')
+  rospy.TopicSub("/mechanism_state", MechanismState, mechanism_callback, frame)
+  rospy.ready('motor_qualification', anonymous=True)
   path=str(packspec.get_pkg_dir('pr2_etherCAT'))
   path=path+'/pr2_etherCAT rteth0 ../../xml/'+xmlFile
   sub = popen2.Popen3(path)
-  frame.pid =sub.pid
   time.sleep(2)
   test_routine(motor,frame)
   
@@ -141,16 +169,32 @@ def test_routine(motor,frame):
   set_controller('test_controller',.02)
   frame.testPassed('motor test 1')
   
-def end_test(pid):
-  grp = os.getpgid(pid)
-  os.killpg(grp, signal.SIGTERM)
-  return 1
+def test1(motor,frame):
+  return
+    
+  
+  
+  
+def end_test():
+  s = rospy.ServiceProxy('shutdown', Empty)
+  s.call(EmptyRequest())
+  return
+  
+def mechanism_callback(data, frame):
+  frame.callback(data)
+ 
 
 if __name__ == '__main__':
-  
-  app = RosApp(0)
-  app.MainLoop()
-  
+  sub = popen2.Popen3('botherder')
+  time.sleep(1)
+  try:
+    app = RosApp(0)
+    app.MainLoop()
+  except:
+    pass
+    
+  os.killpg(os.getpgid(sub.pid), signal.SIGTERM)
+    
   print 'quit'
   rospy.signal_shutdown('GUI shutdown')
 
