4dd2fb14828c91e20007583b4c2832e78a97ddd7
jfaustwg <jfaustwg@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-01-15 02:07:08 +0000

    that'll teach me

--- qualification/src/qualification/ui.py
+++ qualification/src/qualification/ui.py
@@ -434,8 +434,9 @@ class QualificationFrame(wx.Frame):
     
     rework_reason = wx.GetTextFromUser("If this hardware has previously been qualified, and has been reworked in some way, please enter the reason for the re-work", "Rework?", "", self)
     if (len(rework_reason) > 0):
-      invent = get_inventory_client()
-      invent.setNote(self._current_serial, "Hardware rework, reason given: %s"%(rework_reason))
+      invent = self.get_inventory_object()
+      if (invent != None):
+        invent.setNote(self._current_serial, "Hardware rework, reason given: %s"%(rework_reason))
     
     if (self._current_test.getInstructionsFile() != None):
       self.set_top_panel(InstructionsPanel(self._top_panel, self._res, self, os.path.join(test_dir, self._current_test.getInstructionsFile())))
