59fe7b1c09a24a618e5d20f12e680f9ed1f97a4b
wattsk <wattsk@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-03-04 01:51:36 +0000

    Torso life, head life and hot box tests added to life tests

--- life_test/head_test/head_life_test/run_head_test.py
+++ life_test/head_test/head_life_test/run_head_test.py
@@ -33,9 +33,6 @@ PKG = "head_life_test"
 import roslib; roslib.load_manifest(PKG)
 roslib.load_manifest('pr2_mechanism_controllers')
 # Loads interface with the robot.        
-
-
-roslib.load_manifest('mechanism_bringup')
 roslib.load_manifest('mechanism_control')
 
 import sys
@@ -44,24 +41,26 @@ import string
 from time import sleep
 import random
 import rospy
+from std_msgs import *
 from robot_msgs.msg import JointCmd
+from pr2_mechanism_controllers.srv import *
 
 from mechanism_control import mechanism
 
 
 def point_head_client(pan, tilt):
-
     head_angles.publish(JointCmd(['head_pan_joint', 'head_tilt_joint'],[0.0,0.0],[pan, tilt],[0.0, 0.0],[0.0, 0.0]))
     sleep(0.5)
 
 if __name__ == "__main__":
    # spawn head test controller from xml file in pkg
-   head_controller_path = roslib.packages.get_pkg_dir('head_life_test/head_controller.xml')
+   head_controller_path = roslib.packages.get_pkg_dir('head_life_test') + '/head_controller.xml'
    xml_for_head = open(head_controller_path)
    mechanism.spawn_controller(xml_for_head.read())
 
    # run laser controller
    # based off pr2_mechanism_controllers/control_laser.py
+   
    rospy.wait_for_service('laser_controller/set_profile')
    s = rospy.ServiceProxy('laser_controller/set_profile', SetProfile)
    resp = s.call(SetProfileRequest(0.0, 0.0, 0.0, 0.0, 4, 0.25, 0.95, 0.25))
@@ -70,14 +69,27 @@ if __name__ == "__main__":
    rospy.init_node('head_commander', anonymous=True)
    sleep(1)
 
-   while 1:
-       pan = random.uniform(-2.7, 2.7)
-       print pan
-       tilt = random.uniform(-0.3, 0.8)
-       print tilt
-       point_head_client(pan, tilt)
+   try:
+       while not rospy.is_shutdown():
+           pan = random.uniform(-2.7, 2.7)
+           tilt = random.uniform(-0.3, 0.8)
+           point_head_client(pan, tilt)
+   except Exception, e:
+       print 'Caught exception running controllers'
+       print e
+   finally:
+       print 'Killing controllers'
+       for i in range(1,3):
+           try:
+               mechanism.kill_controller('head_controller')
+           except:
+               print 'Failed to kill head controller'
 
-
+       for i in range (1,3):
+           try:
+               mechanism.kill_controller('laser_controller')
+           except:
+               print 'Failed to kill laser controller'
 
 
     
