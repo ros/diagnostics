8ed3622a2dca496715ce30dd88594dde752830cf
gerkey <gerkey@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-05-29 00:48:16 +0000

    Reorganized into a first cut at stacks.

--- life_test/scripts/laser_test.bash
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-`rospack find pr2_mechanism_controllers`/scripts/control_laser.py laser_controller sine 0.25 0.95 0.25
-
--- life_test/scripts/stop_laser_test.bash
+++
@@ -1,2 +0,0 @@
-#
-~/head_tree/ros-pkg/controllers/pr2_mechanism_controllers/scripts/control_laser.py laser_controller sine 0.25 0.0 0.25
