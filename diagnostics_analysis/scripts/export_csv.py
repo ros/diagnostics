80be9c3401fd68793508564859ff93b080c001bc
wattsk <wattsk@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-09-02 00:07:54 +0000

    Allowed different output directories, changing length diagnostics status

--- diagnostics_analysis/scripts/export_csv.py
+++ diagnostics_analysis/scripts/export_csv.py
@@ -32,84 +32,117 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Author: Eric Berger
+# Author: Eric Berger, Kevin Watts
+
+##\brief Converts diagnostics log files into CSV's for analysis
 
 PKG = 'diagnostics_analysis'
 import roslib; roslib.load_manifest(PKG)
-import rosrecord
+import rosrecord #, rostime
 import diagnostic_msgs.msg
-import time, sys
-import traceback
-import os
+import time, sys, os
+import operator, tempfile, subprocess
 
-def init():
-  print "Starting to gather statistics"
-  stats = {}
+from optparse import OptionParser
 
-  return stats
+stats = {}
 
+##\brief Adds message to output file
+def update(topic, msg):
+    global output_dir
 
+    if(not (topic == '/diagnostics')):
+        print "Discarding message on topic: %s" % topic
+        return
 
-def update(stats, topic, msg):
-  global output_dir
-
-  if(not (topic == '/diagnostics')):
-    print "discarding message on topic " + topic
-    return
-  t = time.localtime(float(str(msg.header.stamp)) / 1000000000.0)
+    t = time.localtime(float(str(msg.header.stamp)) / 1000000000.0)
    
-  for status in msg.status:
-    name = status.name
+    for status in msg.status:
+        name = status.name
 
-    if(not stats.has_key(name)):
-      stats[name] = {}
-      stats[name]['fields'] = [s.key for s in status.values]
-      stats[name]['level'] = status.level
-      stats[name]['message'] = status.message
-      
-      file_name = name.replace(' ', '_').replace('(', '').replace(')', '').replace('/', '__').replace('.', '').replace('#', '')
-      stats[name]['file'] = file(output_dir + '/' + file_name + '.csv', 'w')
+        if(not stats.has_key(name)):
+            stats[name] = {}
+            #stats[name]['fields'] = [s.key for s in status.values]
 
-      fields = stats[name]['fields'];
-      stats[name]['file'].write(', '.join(['timestamp'] + ['Level', 'Message'] + 
-                                          [f.replace(',','') for f in fields]) + '\n')
-    
-    # Need stuff for different string fields
-    # Store as dictionary, then convert to CSV?
-    if (not [s.key for s in status.values] == stats[name]['fields']):
-      #print "ERROR, mismatched field names in component %s. Label: %s" %(name, s.key)
-      #return stats
-      continue
+            fields = {}
+            index = 0
+            for s in status.values:
+                fields[s.key] = index
+                index += 1
+            stats[name]['fields'] = fields
 
-    # Should make time machine readable better
-    msg = status.message.replace(',',' ')
+            stats[name]['level'] = status.level
+            stats[name]['message'] = status.message
+            # Use named temp file, will cat this to header on close
+            stats[name]['file'] = tempfile.NamedTemporaryFile()
 
-    stats[name]['file'].write(', '.join([time.asctime(t)] + [str(status.level), msg] + 
-                                        [s.value.replace('\n', ' ').replace(',','') for s in status.values]) + '\n')
+            #fields = stats[name]['fields'];
 
+        # Check to see if fields have changed. Add new fields to map
+        if (not [s.key for s in status.values] == stats[name]['fields'].keys()):
+            for s in status.values:
+                if not stats[name]['fields'].has_key(s.key):
+                    stats[name]['fields'][s.key] = len(stats[name]['fields'])
+                    
+        # Add values in correct place for header index
+        # Key/Value pairs can move around, this makes sure values are added to correct keys
+        vals = []
+        for key, val in stats[name]['fields'].iteritems():
+            vals.append('')
+        for s in status.values:
+            vals[stats[name]['fields'][s.key]] = s.value.replace('\n','  ').replace(',',' ')
+        
+        msg = status.message.replace(',',' ')
+        
+        stats[name]['file'].write(', '.join([time.strftime("%Y/%m/%d %H:%M:%S", t)] + 
+                                            [str(status.level), msg] + vals) + '\n')
 
-def output(stats):
-  for name in stats:
-    stats[name]['file'].close()
+##\brief Saves output file with complete header row
+def finish_file():
+    for name in stats:
+        # Sort fields by correct index, add to header
+        field_dict = sorted(stats[name]['fields'].iteritems(), key=operator.itemgetter(1))
+        fields = map(operator.itemgetter(0), field_dict)
 
+        header_line = ', '.join(['Timestamp'] + ['Level', 'Message'] + [f.replace(',','').replace('\n', ' ') for f in fields]) + '\n'
 
-def process_bag(stats, update, bagfile):
-  for (topic, msg, t) in rosrecord.logplayer(bagfile, raw=False):
-    update(stats, topic, msg)
+        file_name = os.path.join(output_dir, name.replace(' ', '_').replace('(', '').replace(')', '').replace('/', '__').replace('.', '').replace('#', '') + '.csv')
+        
+        output_file = file(file_name, 'w')
+        output_file.write(header_line)
+        output_file.close()
+
+        # Cat the temp data to the header file and close it
+        subprocess.call("cat %s >> %s" % (stats[name]['file'].name, file_name), shell=True)
+        stats[name]['file'].close() # Destroy temp file
+
+##\brief Plays bagfile and updates logs with new messages
+def process_bag(bagfile):
+    for (topic, msg, t) in rosrecord.logplayer(bagfile, raw=False):
+        update(topic, msg)
+
 
 if __name__ == '__main__':
-  stats = init()
-  try:
-    for i, f in enumerate(sys.argv[1:]): 
-      global output_dir
-      filepath = 'output/%s_csv' % os.path.basename(f)[0:os.path.basename(f).find('.')]
-      
-      output_dir = os.path.join(roslib.packages.get_pkg_dir(PKG),  filepath)
-      print "(%d) processing file: %s. Output dir: %s" %(i, f, filepath) 
-      if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
+    # Allow user to set output directory
+    parser = OptionParser()
+    parser.add_option("-d", "--directory", dest="directory",
+                      help="Write output to DIR. Default: %s" % PKG, metavar="DIR",
+                      default=roslib.packages.get_pkg_dir(PKG), action="store")
+    options, args = parser.parse_args()
 
-      process_bag(stats, update, f)
-      output(stats)
-  except:
-    traceback.print_exc()
+    try:
+        for i, f in enumerate(args):
+            global output_dir
+            filepath = 'output/%s_csv' % os.path.basename(f)[0:os.path.basename(f).find('.')]
+            
+            output_dir = os.path.join(options.directory,  filepath)
+            print "(%d) processing file: %s. Output dir: %s/output" %(i, f, options.directory)
+            if not os.path.isdir(output_dir):
+                os.makedirs(output_dir)
+                
+            process_bag(f)
+            finish_file()
+    except:
+        import traceback
+        print "Caught exceptiong processing log file"
+        traceback.print_exc()
