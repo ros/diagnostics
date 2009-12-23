af3fd516d122c0dbaac3a5ebda87fd9aed3a69a4
watts <watts@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-12-23 02:56:18 +0000

    Added stale entry to color dictionary for robot monitor viewer panel

--- robot_monitor/src/robot_monitor/viewer_panel.py
+++ robot_monitor/src/robot_monitor/viewer_panel.py
@@ -55,7 +55,7 @@ import cStringIO
 from message_timeline import MessageTimeline
 
 stat_dict = {0: 'OK', 1: 'Warning', 2: 'Error', 3: 'Stale' }
-color_dict = {0: wx.Colour(85, 178, 76), 1: wx.Colour(222, 213, 17), 2: wx.Colour(178, 23, 46)}
+color_dict = {0: wx.Colour(85, 178, 76), 1: wx.Colour(222, 213, 17), 2: wx.Colour(178, 23, 46), 3: wx.Colour(178, 23, 46)}
 
 class SnapshotFrame(wx.Frame):
     def __init__(self, parent, name):
