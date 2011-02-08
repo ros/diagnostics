0a02e222d510d8b1a89d697e8d3bb17bc1cefec1
watts <watts@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2011-02-08 22:26:15 +0000

    Removing GUI tools from diagnostics stack

--- robot_monitor/src/robot_monitor/__init__.py
+++
@@ -1,7 +0,0 @@
-WXVER = '2.8'
-import wxversion
-if wxversion.checkInstalled(WXVER):
-  wxversion.select(WXVER)
-else:
-  print >> sys.stderr, "This application requires wxPython version %s"%(WXVER)
-  sys.exit(1)
