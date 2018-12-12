438607b3cdf637fe8a25d5315cc4d728494b2eef
VaibhavBhadade <vaibhav_bhadade@persistent.com>
2018-12-12 19:46:05 +0530

    Removed old files for ros2 poritng

--- diagnostic_analysis/diagnostic_analysis/sparse_csv.py
+++ diagnostic_analysis/diagnostic_analysis/sparse_csv.py
@@ -1,46 +1,22 @@
-#!/usr/bin/python
-# 
-# Software License Agreement (BSD License)
+#!/usr/bin/python3
+# Copyright 2015 Open Source Robotics Foundation, Inc.
 #
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of the Willow Garage nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Author: Kevin Watts
-
 
 # Make any csv into sparse csv
 
-
 PKG = 'diagnostic_analysis'
-#import roslib
-#roslib.load_manifest(PKG)
 
 import csv, os, sys
 from optparse import OptionParser
@@ -55,9 +31,9 @@ import csv, os, sys
 def make_sparse_skip(csv_file, skip):
     output_file = csv_file[:-4] + '_sparse.csv'
 
-    input_reader = csv.reader(open(csv_file, 'rb'))
+    input_reader = csv.reader(open(csv_file, 'r', encoding = "utf8"))
 
-    f = open(output_file, 'wb')
+    f = open(output_file, 'w')
     output_writer = csv.writer(f)
 
     skip_count = skip
@@ -77,9 +53,9 @@ def make_sparse_skip(csv_file, skip):
 def make_sparse_length(csv_file, length):
     output_file = csv_file[:-4] + '_sprs_len.csv'
 
-    input_reader = csv.reader(open(csv_file, 'rb'))
+    input_reader = csv.reader(open(csv_file, 'r',encoding = "utf8"))
 
-    f = open(output_file, 'wb')
+    f = open(output_file, 'w')
     output_writer = csv.writer(f)
 
     # Calculate skip count for file
