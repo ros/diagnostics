92e35a925a3fafd7ccf8d4d125ebd636718234de
Scott K Logan <logans@cottsay.net>
2019-08-23 00:00:40 -0700

    Update diagnostic_updater/src/example.cpp
    Co-Authored-By: Karsten Knese <Karsten1987@users.noreply.github.com>

--- diagnostic_updater/src/example.cpp
+++ diagnostic_updater/src/example.cpp
@@ -244,7 +244,7 @@ int main(int argc, char ** argv)
     // We can call updater.update whenever is convenient. It will take care
     // of rate-limiting the updates.
     updater.update();
-    rclcpp::spinSome(node);
+    rclcpp::spin_some(node);
   }
 
   return 0;
