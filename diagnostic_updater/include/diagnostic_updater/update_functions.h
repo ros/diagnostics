ac1e3661cf0ba435eda72665a20e33b5b0ac2a1b
seiya0412 <seiya.shimizu@whill.inc>
2020-05-02 16:54:30 +0900

    Add min/max disired freq as a field

--- diagnostic_updater/include/diagnostic_updater/update_functions.h
+++ diagnostic_updater/include/diagnostic_updater/update_functions.h
@@ -263,14 +263,20 @@ public:
     if(*params_.min_freq_ == *params_.max_freq_)
       stat.addf("Target frequency (Hz)", "%f", *params_.min_freq_);
     if(*params_.min_freq_ > 0)
+    {
       stat.addf("Minimum acceptable frequency (Hz)", "%f", *params_.min_freq_ * (1 - params_.tolerance_));
+      stat.addf("Minimum desired frequency (Hz)", "%f", *params_.min_freq_);
+    }
 
 #ifdef _WIN32
     if(isfinite(*params_.max_freq_))
 #else
     if(finite(*params_.max_freq_))
 #endif
+    {
       stat.addf("Maximum acceptable frequency (Hz)", "%f", *params_.max_freq_ * (1 + params_.tolerance_));
+      stat.addf("Maximum desired frequency (Hz)", "%f", *params_.max_freq_);
+    }
   }
 };
 
