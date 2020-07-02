d39c1004a78b9dd725c45ea58802d4e8d6a43de3
Mikael Arguedas <mikael.arguedas@gmail.com>
2020-07-02 08:23:19 +0200

    fix syntax error
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- diagnostic_common_diagnostics/src/diagnostic_common_diagnostics/sensors_monitor.py
+++ diagnostic_common_diagnostics/src/diagnostic_common_diagnostics/sensors_monitor.py
@@ -217,7 +217,7 @@ if __name__ == '__main__':
     try:
         rospy.init_node('sensors_monitor_%s'%hostname_clean)
     except rospy.ROSInitException:
-        print(file=sys.stderr, 'Unable to initialize node. Master may not be running')
+        print('Unable to initialize node. Master may not be running', file=sys.stderr)
         sys.exit(0)
 
     monitor = SensorsMonitor(hostname)
