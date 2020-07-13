634acf811f8c7966eab0360566e22530e7048403
Rein Appeldoorn <reinzor@gmail.com>
2020-07-13 11:28:51 +0200

    feat(cpu_monitor): Window averaging

--- diagnostic_common_diagnostics/src/diagnostic_common_diagnostics/cpu_monitor.py
+++ diagnostic_common_diagnostics/src/diagnostic_common_diagnostics/cpu_monitor.py
@@ -34,35 +34,45 @@
 
 # \author Rein Appeldoorn
 
-import rospy
-from diagnostic_updater import DiagnosticTask, Updater
-from diagnostic_msgs.msg import DiagnosticStatus
+import collections
+import socket
 
 import psutil
-import socket
+import rospy
+from diagnostic_msgs.msg import DiagnosticStatus
+from diagnostic_updater import DiagnosticTask, Updater
 
 
 class CpuTask(DiagnosticTask):
-    def __init__(self, warning_percentage):
+    def __init__(self, warning_percentage, window):
         DiagnosticTask.__init__(self, "CPU Information")
         self._warning_percentage = int(warning_percentage)
+        self._readings = collections.deque(maxlen=window)
+
+    def _get_average_reading(self):
+        def avg(lst):
+            return float(sum(lst)) / len(lst)
+
+        return [avg(cpu_percentages) for cpu_percentages in zip(*self._readings)]
 
     def run(self, stat):
-        cpu_percentages = psutil.cpu_percent(percpu=True)
+        self._readings.append(psutil.cpu_percent(percpu=True))
+        cpu_percentages = self._get_average_reading()
         cpu_average = sum(cpu_percentages) / len(cpu_percentages)
 
-        stat.add("CPU Load Average", cpu_average)
+        stat.add("CPU Load Average", "{:.2f}".format(cpu_average))
 
         warn = False
         for idx, val in enumerate(cpu_percentages):
-            stat.add("CPU {} Load".format(idx), "{}".format(val))
+            stat.add("CPU {} Load".format(idx), "{:.2f}".format(val))
             if val > self._warning_percentage:
                 warn = True
 
         if warn:
-            stat.summary(DiagnosticStatus.WARN, "At least one CPU exceeds %d percent" % self._warning_percentage)
+            stat.summary(DiagnosticStatus.WARN,
+                         "At least one CPU exceeds {:d} percent".format(self._warning_percentage))
         else:
-            stat.summary(DiagnosticStatus.OK, "CPU Average %.1f percent" % cpu_average)
+            stat.summary(DiagnosticStatus.OK, "CPU Average {:.2f} percent".format(cpu_average))
 
         return stat
 
@@ -73,7 +83,7 @@ def main():
 
     updater = Updater()
     updater.setHardwareID(hostname)
-    updater.add(CpuTask(rospy.get_param("~warning_percentage", 90)))
+    updater.add(CpuTask(rospy.get_param("~warning_percentage", 90), rospy.get_param("~window", 1)))
 
     rate = rospy.Rate(rospy.get_param("~rate", 1))
     while not rospy.is_shutdown():
