183362675c9b16075fa7796d84fa6210c737e5b5
Ryan Friedman <ryan_friedman@trimble.com>
2022-11-23 11:11:31 -0700

    Add missing timestamp for header from ROS2 port
    Signed-off-by: Ryan Friedman <ryan_friedman@trimble.com>

--- diagnostic_common_diagnostics/diagnostic_common_diagnostics/ntp_monitor.py
+++ diagnostic_common_diagnostics/diagnostic_common_diagnostics/ntp_monitor.py
@@ -129,7 +129,7 @@ class NTPMonitor(Node):
 
     def checkCB(self):
         new_msg = DIAG.DiagnosticArray()
-        # new_msg.header.stamp = rospy.get_rostime()
+        new_msg.header.stamp = self.get_clock().now().to_msg()
 
         st = self.ntp_diag(self.stat)
         if st is not None:
