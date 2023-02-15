2707b64905b1312783b8c91caaa16418a7ad0376
Christian Henkel <christian.henkel2@de.bosch.com>
2023-02-15 16:21:55 +0100

    removing copy constructor
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- diagnostic_updater/include/diagnostic_updater/diagnostic_status_wrapper.hpp
+++ diagnostic_updater/include/diagnostic_updater/diagnostic_status_wrapper.hpp
@@ -79,7 +79,7 @@ public:
    * nothing.
    * \param other Reference to object to copy
    */
-  explicit DiagnosticStatusWrapper(const DiagnosticStatusWrapper & other) = default;
+  explicit DiagnosticStatusWrapper(const DiagnosticStatusWrapper & other) = delete;
 
   /**
    * \brief Fills out the level and message fields of the DiagnosticStatus.
