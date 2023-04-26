1c3ca747dcfae62412d77b43ce80f48bcf3a6db1
robo <robo2020@gmail.com>
2023-04-26 23:19:05 +0300

    format and run linters

--- ros2diagnostics_cli/ros2diagnostics_cli/verb/list.py
+++ ros2diagnostics_cli/ros2diagnostics_cli/verb/list.py
@@ -30,10 +30,9 @@
 from ros2cli.verb import VerbExtension
 from ros2diagnostics_cli.api import DiagnosticsParser, ParserModeEnum
 
+
 class ListVerb(VerbExtension):
     """List all diagnostic status items group by node name"""
-
-
 
     def main(self, *, args):
         diagnostic_parser = DiagnosticsParser(ParserModeEnum.List)
