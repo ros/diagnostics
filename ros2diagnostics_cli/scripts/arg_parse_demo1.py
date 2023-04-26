f637fadf69c70f1b1d15f0a07d05de037cf8b5bd
robo <robo2020@gmail.com>
2023-04-26 22:50:08 +0300

    clean test and scripts file

--- ros2diagnostics_cli/scripts/arg_parse_demo1.py
+++
@@ -1,6 +0,0 @@
-from argparse import ArgumentParser, Namespace
-
-parser = ArgumentParser()
-parser.add_argument("echo", help="echo given string")
-args: Namespace = parser.parse_args()
-print(args.echo)
--- ros2diagnostics_cli/scripts/arg_parse_demo2.py
+++
@@ -1,6 +0,0 @@
-from argparse import ArgumentParser, Namespace
-
-parser = ArgumentParser()
-parser.add_argument("square", help="square given number", type=int)
-args: Namespace = parser.parse_args()
-print(args.square**2)
