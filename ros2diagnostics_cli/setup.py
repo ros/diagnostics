ddfba3bcb237d05ad5d4da9f6e0acbf6d065c59a
robo <robo2020@gmail.com>
2023-05-04 21:21:02 +0300

    set version and license

--- ros2diagnostics_cli/setup.py
+++ ros2diagnostics_cli/setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 from setuptools import find_packages
 
-package_name = 'ros2diagnostics_cli'
+PACKAGE_NAME = 'ros2diagnostics_cli'
 
 setup(
-    name=package_name,
-    version='0.0.1',
+    name=PACKAGE_NAME,
+    version='3.1.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+            ['resource/' + PACKAGE_NAME]),
+        ('share/' + PACKAGE_NAME, ['package.xml']),
     ],
     install_requires=['ros2cli'],
     zip_safe=True,
     maintainer='user',
     maintainer_email='robo2020@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='diagnostic command for ROS2 command line, parse and show /diagnostics topic',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'ros2cli.command': [
