103873fb596fbfc67404cfb17584ff46627334e8
Kevin Schwarzer <kevin.anschauschwarzer+github@gmail.com>
2023-07-05 16:05:42 +0200

    test: check the logs for a warning when updater is verbose
    Add a dummy process, which wraps a `Node` with a verbose `Updater`. Use `launch_testing` to check the process' stdout for a certain string.

---
+++ diagnostic_updater/test/dummy_process.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import rclpy
+from diagnostic_updater import Updater
+
+
+def main():
+    rclpy.init()
+
+    node = rclpy.create_node('talker')
+    updater = Updater(node)
+    updater.add('do_nothing', lambda stat: stat)
+    updater.verbose = True
+    node.create_timer(0.1, lambda: updater.update())
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.try_shutdown()
+
+if __name__ == '__main__':
+    main()
