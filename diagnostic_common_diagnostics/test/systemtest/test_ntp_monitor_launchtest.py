b144e882ea4a24f705b25e258e718036defc0025
Christian Henkel <christian.henkel2@de.bosch.com>
2024-05-14 11:09:08 +0200

    bigger offsets allowed
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- diagnostic_common_diagnostics/test/systemtest/test_ntp_monitor_launchtest.py
+++ diagnostic_common_diagnostics/test/systemtest/test_ntp_monitor_launchtest.py
@@ -58,10 +58,10 @@ def generate_test_description():
             executable='ntp_monitor.py',
             name='ntp_monitor',
             output='screen',
-            arguments=['--offset-tolerance', '10000',
-                       '--error-offset-tolerance', '20000',
+            arguments=['--offset-tolerance', '100000',
+                       '--error-offset-tolerance', '200000',
                        '--ntp_hostname', 'ntp.ubuntu.com']
-            # 10s, 20s, we are not testing if your clock is correct
+            # 100s, 200s, we are not testing if your clock is correct
         ),
         launch_testing.actions.ReadyToTest()
     ])
