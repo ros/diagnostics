248a242e92f6c4003482555593337585ba41dcd3
Antoine Lima <antoine@sagou.in>
2024-06-03 18:13:56 +0200

    Fix hd_monitor test

--- diagnostic_common_diagnostics/test/systemtest/test_hd_monitor_launchtest.py
+++ diagnostic_common_diagnostics/test/systemtest/test_hd_monitor_launchtest.py
@@ -59,7 +59,7 @@ def generate_test_description():
                 executable="hd_monitor.py",
                 name="hd_monitor",
                 output="screen",
-                ros_arguments=["free_percent_low", "0.20", "free_percent_crit", "0.05"],
+                parameters=[{"free_percent_low": 0.20, "free_percent_crit": 0.05}],
             ),
             launch_testing.actions.ReadyToTest(),
         ]
