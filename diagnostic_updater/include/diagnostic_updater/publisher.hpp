d97e932c2fdb37e2710b80fddf3479ac7dd2d88f
homalozoa <nx.tardis@gmail.com>
2024-10-29 12:38:13 +0800

    lint: reformat code
    Signed-off-by: homalozoa <nx.tardis@gmail.com>

--- diagnostic_updater/include/diagnostic_updater/publisher.hpp
+++ diagnostic_updater/include/diagnostic_updater/publisher.hpp
@@ -242,7 +242,9 @@ public:
    * The timestamp to be used by the TimeStampStatus class will be
    * extracted from message.header.stamp.
    */
-  virtual void publish(std::unique_ptr<typename PublisherT::ROSMessageType, typename PublisherT::ROSMessageTypeDeleter> message)
+  virtual void publish(
+    std::unique_ptr<typename PublisherT::ROSMessageType,
+    typename PublisherT::ROSMessageTypeDeleter> message)
   {
     tick(message->header.stamp);
     publisher_->publish(std::move(message));
