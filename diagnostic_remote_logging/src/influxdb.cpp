05c1ca90a04c400be4ba6154bf228e761290ea12
Daan Wijffels <dwijffels@lely.com>
2025-08-28 15:12:06 +0200

    Removed the use of send.agg, now aggregate all /diagnostics and send them on a timer

--- diagnostic_remote_logging/src/influxdb.cpp
+++ diagnostic_remote_logging/src/influxdb.cpp
@@ -172,8 +172,8 @@ bool InfluxDB::sendToInfluxDB(const std::string & data)
   curl_easy_getinfo(curl_, CURLINFO_RESPONSE_CODE, &response_code);
 
   if (response_code != 204) {
-    RCLCPP_ERROR(
-      this->get_logger(), "Error (%ld) when sending to telegraf:\n%s", response_code, data.c_str());
+    RCLCPP_ERROR(this->get_logger(), "Error (%d) when sending to telegraf:\n%s",
+                 response_code, data.c_str());
     return false;
   }
 
