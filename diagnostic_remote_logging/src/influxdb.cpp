42a159b80fed794fc97fec8d194dd2d9fa51bec5
Daan Wijffels <dwijffels@lely.com>
2025-08-28 15:16:04 +0200

    Removed the use of send.agg, now aggregate all /diagnostics and send them on a timer

--- diagnostic_remote_logging/src/influxdb.cpp
+++ diagnostic_remote_logging/src/influxdb.cpp
@@ -85,7 +85,7 @@ InfluxDB::InfluxDB(const rclcpp::NodeOptions & opt)
       std::bind(&InfluxDB::diagnosticsCallback, this, std::placeholders::_1));
   }
 
-  if (declare_parameter("send.agg", true)) {
+  if (declare_parameter("send.agg", false)) {
     throw std::runtime_error(
       "The option send.agg is deprecated and will be removed in a future version. Use "
       "send.diagnostics and send.period instead.");
@@ -172,8 +172,17 @@ bool InfluxDB::sendToInfluxDB(const std::string & data)
   curl_easy_getinfo(curl_, CURLINFO_RESPONSE_CODE, &response_code);
 
   if (response_code != 204) {
-    RCLCPP_ERROR(this->get_logger(), "Error (%d) when sending to telegraf:\n%s",
-                 response_code, data.c_str());
+      << << << < HEAD
+      RCLCPP_ERROR(
+      this->get_logger(), "Error (%d) when sending to telegraf:\n%s", response_code, data.c_str());
+    ==
+    ==
+    ==
+      =
+      RCLCPP_ERROR(
+      this->get_logger(), "Error (%d) when sending to telegraf:\n%s", response_code, data.c_str());
+>> >> >> > ae5d7e4 (Removed the use of send.agg,
+      now aggregate all / diagnostics and send them on a timer)
     return false;
   }
 
