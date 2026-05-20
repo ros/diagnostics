b537c9f2d697f12498b5ff1b5be8755ae0ad07be
Christian Henkel <christian.henkel2@de.bosch.com>
2026-05-20 18:19:52 +0200

    trying if the linters lint
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- diagnostic_updater/test/test_diagnostic_status_wrapper.py
+++ diagnostic_updater/test/test_diagnostic_status_wrapper.py
@@ -13,6 +13,8 @@ from diagnostic_updater import DiagnosticStatusWrapper
 class TestDiagnosticStatusWrapper(unittest.TestCase):
 
     def test_init_empty(self):
+        """breaking convnetions
+        """
         d = DiagnosticStatusWrapper()
         self.assertEqual(d.level, DiagnosticStatus.OK)
         self.assertEqual(d.message, '')
