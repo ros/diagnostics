ce6b646a8e0ac90555a8a1aeafa792ab73838a18
Christian Henkel <christian.henkel2@de.bosch.com>
2026-05-21 01:33:12 +0200

    flakes
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- diagnostic_topic_monitor/test/test_topic_frequency_monitor.py
+++ diagnostic_topic_monitor/test/test_topic_frequency_monitor.py
@@ -6,28 +6,29 @@
 
 import time
 import unittest
-import pytest
 
+from diagnostic_msgs.msg import DiagnosticArray
+from diagnostic_msgs.msg import DiagnosticStatus
 import launch
-from launch.actions import EmitEvent, RegisterEventHandler, LogInfo, SetEnvironmentVariable
+from launch.actions import EmitEvent
+from launch.actions import LogInfo
+from launch.actions import RegisterEventHandler
+from launch.actions import SetEnvironmentVariable
 import launch.event_handlers.on_process_start
-
-from launch_ros.actions import LifecycleNode, Node
-from launch_ros.events.lifecycle import ChangeState
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import LifecycleNode
+from launch_ros.actions import Node
 import launch_ros.events.lifecycle
-
+from launch_ros.events.lifecycle import ChangeState
+from launch_ros.substitutions import FindPackageShare
 import launch_testing
 import launch_testing.actions
 import launch_testing.asserts
-
 from lifecycle_msgs.msg import Transition
-from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
-
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-
+import pytest
 import rclpy
-from rclpy.time import Time, Duration
+from rclpy.time import Duration
+from rclpy.time import Time
 
 
 def create_change_state(target, target_state):
@@ -212,7 +213,7 @@ class TestMonitor(unittest.TestCase):
         # check some fields for present/content
         self.assertTrue(CONFIG_MONITOR_NAME in status.name)
         keys = [kv.key for kv in status.values]
-        self.assertIn("Actual frequency (Hz)", keys)
+        self.assertIn('Actual frequency (Hz)', keys)
 
     def test_ignore_unconfigured(self):
         """Check that we ignore the topic we don't monitor."""
