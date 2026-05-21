e0700a9646606140c525cf40a7c169de05fbe486
Christian Henkel <6976069+ct2034@users.noreply.github.com>
2026-05-21 12:04:44 +0200

    Feature topic monitor (#605)
    * Original topic monitor version for galactic
    
    * Rename to diagnostic_topic_monitor
    
    * Refactor code and package (#1)
    
    * Add topic monitor components to diagnostics
    
    Co-authored-by: Tejas Kumar Shastha <tejas.kumarshastha@de.bosch.com>
    Signed-off-by: Ingo Lütkebohle <ingo.luetkebohle@de.bosch.com>
    
    * Fix license tag
    
    * Add new package to lint and test
    
    * Fix copyright notices
    
    * author under license
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * flake8
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * cpplint
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * quotes
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * '
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * target_link_libraries
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * imperative
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * flake8
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * flakes
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * whitespace
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * windows edge case
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * windows why
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * may also be bigger
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * issues w/ defines
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * windows
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * fixes #592 and #526
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * missing test dep
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * three different publishers
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * taking more time
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * testing for all 4 topics
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * this did lead to -1 as lowest level in corner cases
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    * fix
    
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    
    ---------
    
    Signed-off-by: Ingo Lütkebohle <ingo.luetkebohle@de.bosch.com>
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>
    Co-authored-by: Ingo Lütkebohle (CR/ASD1) <ingo.luetkebohle@de.bosch.com>
    Co-authored-by: Tejas Kumar Shastha <tejas.shastha@gmail.com>
    Co-authored-by: Tejas Kumar Shastha <tejas.kumarshastha@de.bosch.com>
    (cherry picked from commit 207fe69ce1dfd1ec7f443f392da7ebb16d2ebf57)

---
+++ diagnostic_topic_monitor/test/dummy_publishers.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2024, 2025 Robert Bosch GmbH
+#
+# See the top-level LICENSE file for licensing terms.
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import String
+
+
+class DummyPublisherNode(Node):
+
+    def __init__(self):
+        super().__init__('dummy_publisher_node')
+
+        # Publishers
+        self.string_pub1 = self.create_publisher(String, '/dummy_string_topic1', 10)
+        self.string_pub2 = self.create_publisher(String, '/dummy_string_topic2', 10)
+        self.string_pub3 = self.create_publisher(String, '/dummy_string_topic3', 10)
+        # For testing the age_monitor, we need any topic with a header
+        self.header_pub = self.create_publisher(CameraInfo, '/dummy_header_topic', 10)
+
+        # Timer to publish at 1Hz
+        self.timer = self.create_timer(1.0, self.publish_messages)
+
+        self.get_logger().info('DummyPublisherNode has been started.')
+
+    def publish_messages(self):
+        # Publish to /dummy_string_topic1
+        msg1 = String()
+        msg1.data = 'Hello from dummy_string_topic1'
+        self.string_pub1.publish(msg1)
+
+        # Publish to /dummy_string_topic2
+        msg2 = String()
+        msg2.data = 'Hello from dummy_string_topic2'
+        self.string_pub2.publish(msg2)
+
+        # Publish to /dummy_string_topic3
+        msg3 = String()
+        msg3.data = 'Hello from dummy_string_topic3'
+        self.string_pub3.publish(msg3)
+
+        # Publish to /dummy_header_topic
+        header_msg = CameraInfo()
+        header_msg.header.stamp = self.get_clock().now().to_msg()
+        header_msg.header.frame_id = 'dummy_frame'
+        self.header_pub.publish(header_msg)
+
+        self.get_logger().debug('Published dummy messages to all topics.')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DummyPublisherNode()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Shutting down DummyPublisherNode...')
+
+
+if __name__ == '__main__':
+    main()
