c1e8ca5fadf26025451e4598268e68580811cfb1
Christian Henkel <christian.henkel2@de.bosch.com>
2026-05-21 10:52:44 +0200

    missing test dep
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- diagnostic_topic_monitor/test/dummy_publishers.py
+++ diagnostic_topic_monitor/test/dummy_publishers.py
@@ -19,6 +19,7 @@ class DummyPublisherNode(Node):
         self.string_pub1 = self.create_publisher(String, '/dummy_string_topic1', 10)
         self.string_pub2 = self.create_publisher(String, '/dummy_string_topic2', 10)
         self.string_pub2 = self.create_publisher(String, '/dummy_string_topic3', 10)
+        # For testing the age_monitor, we need any topic with a header
         self.header_pub = self.create_publisher(CameraInfo, '/dummy_header_topic', 10)
 
         # Timer to publish at 10Hz
@@ -27,22 +28,22 @@ class DummyPublisherNode(Node):
         self.get_logger().info('DummyPublisherNode has been started.')
 
     def publish_messages(self):
-        # Publish to /string_topic1
+        # Publish to /dummy_string_topic1
         msg1 = String()
-        msg1.data = 'Hello from string_topic1'
+        msg1.data = 'Hello from dummy_string_topic1'
         self.string_pub1.publish(msg1)
 
-        # Publish to /string_topic2
+        # Publish to /dummy_string_topic2
         msg2 = String()
-        msg2.data = 'Hello from string_topic2'
+        msg2.data = 'Hello from dummy_string_topic2'
         self.string_pub2.publish(msg2)
 
-        # Publish to /string_topic3
+        # Publish to /dummy_string_topic3
         msg3 = String()
-        msg3.data = 'Hello from string_topic3'
+        msg3.data = 'Hello from dummy_string_topic3'
         self.string_pub2.publish(msg3)
 
-        # Publish to /header_topic
+        # Publish to /dummy_header_topic
         header_msg = CameraInfo()
         header_msg.header.stamp = self.get_clock().now().to_msg()
         header_msg.header.frame_id = 'dummy_frame'
